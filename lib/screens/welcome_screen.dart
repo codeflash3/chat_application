c2741d9dc485930a777f545dcc6d2a2b42e71f28
shripad2001 <62115866+shripad2001@users.noreply.github.com>
2021-12-09 00:56:22 +0530

    first commit

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'registration_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/rounded_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(
+              title: 'Log In',
+              colour: Colors.lightBlueAccent,
+              onPressed: () {
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+            ),
+            RoundedButton(
+              title: 'Register',
+              colour: Colors.blueAccent,
+              onPressed: () {
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+// import 'package:flash_chat/screens/login_screen.dart';
+// import 'package:flash_chat/screens/registration_screen.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'login_screen.dart';
+// import 'registration_screen.dart';
+// import 'package:animated_text_kit/animated_text_kit.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+//
+// class WelcomeScreen extends StatefulWidget {
+//   static String id = 'welcome_screen';
+//
+//   @override
+//   _WelcomeScreenState createState() => _WelcomeScreenState();
+// }
+//
+// class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+//
+//   AnimationController controller;
+//   @override
+//   void initState() {
+//     super.initState();
+//     controller = AnimationController(
+//       duration: Duration(seconds: 2),
+//       vsync: this,
+//     );
+//     controller.forward();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Padding(
+//         padding: EdgeInsets.symmetric(horizontal: 24.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: <Widget>[
+//             Row(
+//               children: <Widget>[
+//                 Hero(
+//                   tag: 'logo',
+//                   child: Container(
+//                     child: Image.asset('images/logo.png'),
+//                     height: 60.0,
+//                   ),
+//                 ),
+//                 TypewriterAnimatedTextKit(
+//                   text :['Flash Chat'],
+//                   textStyle: TextStyle(
+//                     fontSize: 45.0,
+//                     fontWeight: FontWeight.w900,
+//                     color: Colors.black,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             SizedBox(
+//               height: 48.0,
+//             ),
+//             Padding(
+//               padding: EdgeInsets.symmetric(vertical: 16.0),
+//               child: Material(
+//                 elevation: 5.0,
+//                 color: Colors.lightBlueAccent,
+//                 borderRadius: BorderRadius.circular(30.0),
+//                 child: MaterialButton(
+//                   onPressed: () {
+//                     Navigator.pushNamed(context, LoginScreen.id);
+//                   },
+//                   minWidth: 200.0,
+//                   height: 42.0,
+//                   child: Text(
+//                     'Log In',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 20.0,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             Padding(
+//               padding: EdgeInsets.symmetric(vertical: 16.0),
+//               child: Material(
+//                 color: Colors.blueAccent,
+//                 borderRadius: BorderRadius.circular(30.0),
+//                 elevation: 5.0,
+//                 child: MaterialButton(
+//                   onPressed: () {
+//                     Navigator.pushNamed(context, RegistrationScreen.id);
+//                   },
+//                   minWidth: 200.0,
+//                   height: 42.0,
+//                   child: Text(
+//                     'Register',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 20.0,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
